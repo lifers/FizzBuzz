eca1539f2e356784c955fe6aeb180262e4e3f9e2
Nagata Parama Aptana <61454123+lifers@users.noreply.github.com>
2023-09-11 20:09:34 -0400

    Fix indexing in FizzBuzz.java

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -6,7 +6,7 @@ class FizzBuzz {
     public static void main(String[] args) {
 
         int i = 1;
-        while (i < 100) {
+        while (i <= 100) {
 
             doFizzBuzz(i);
             i++;
